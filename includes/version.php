6f5b148ed816fe481153d335c3fc19168fb0e6ea
Pete Donnell <pete.donnell@port.ac.uk>
2013-03-27 19:39:30 +0000

    Added version information

---
+++ includes/version.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * CoNtRol version number
+ *
+ * @author     Pete Donnell <pete dot donnell at port dot ac dot uk>
+ * @copyright  University of Portsmouth 2013
+ * @license    https://gnu.org/licenses/gpl-3.0-standalone.html
+ * @created    27/03/2013
+ * @modified   27/03/2013
+ */
+define('CONTROL_VERSION', '0.9.0', false);
